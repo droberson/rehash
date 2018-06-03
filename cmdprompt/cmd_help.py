666f15bd35d9eaa1ea51965828d993862f6aca13
Daniel Roberson <daniel@planethacker.net>
2018-06-03 11:31:47 -0700

    line up output for easy reading.

--- cmdprompt/cmd_help.py
+++ cmdprompt/cmd_help.py
@@ -1,8 +1,18 @@
+"""
+help command.
+
+Prints out the help menu at rehash> prompt
+
+TODO: readline support for tab completion.
+TODO: in-depth help for specific commands: help <command>
+"""
+
 from .commands import *
 
 def cmd_help(sock, cmdline):
+    maxlen = max([len(x[0]) for x in commandhelp])
     for item in commandhelp:
-        print("%s - %s" % (item[0], item[1]))
+        print("%s - %s" % (item[0].ljust(maxlen), item[1]))
     prompt(sock)
 
 commands.append(("help", cmd_help))

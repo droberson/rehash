87c671c3ca164516dea5b24b8088773f4fd188fe
Daniel Roberson <daniel@planethacker.net>
2018-06-03 11:11:54 -0700

    pylint issues + comment header

--- cmdprompt/__init__.py
+++ cmdprompt/__init__.py
@@ -1,11 +1,17 @@
+"""
+This should import every file named cmd_*.py in this directory.
+
+Each file will be a command for the rehash> prompt.
+"""
+
 import os
 import glob
 
 from .commands import *
 
-modules = glob.glob(os.path.dirname(__file__) + "/cmd_*.py")
-modules = ["." + os.path.basename(f)[:-3] for f in modules if os.path.isfile(f)]
+MODULES = glob.glob(os.path.dirname(__file__) + "/cmd_*.py")
+MODULES = ["." + os.path.basename(f)[:-3] for f in MODULES if os.path.isfile(f)]
 
 # l-o-l. Couldn't figure out a better way to do this.
-for module in modules:
+for module in MODULES:
     exec("from " + module + " import *")

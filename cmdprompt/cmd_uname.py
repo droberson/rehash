703204874351e0d3704ac5cf89959e6f5003415f
Daniel Roberson <daniel@planethacker.net>
2018-06-03 11:18:03 -0700

    style changes

--- cmdprompt/cmd_uname.py
+++ cmdprompt/cmd_uname.py
@@ -1,14 +1,13 @@
-'''
+"""
 This is useless. Just a simple example of adding a command to the prompt.
-'''
+"""
+
 from .commands import *
 
 
 def cmd_uname(sock, cmdline):
-    print("cmdline=%s" % cmdline)
     uname = "uname -a" + "\n"
-    print("uname=%s" % uname.encode())
     sock.send(uname.encode())
 
 commands.append(("uname", cmd_uname))
-commandhelp.append(("uname", "executes uname -a on a system"))
+commandhelp.append(("uname", "executes uname -a on a system (example command)"))
